54ce39d654fab4dafc5e995781493a948225f395
Keisuke Shirato <shirato.engineer@gmail.com>
2024-03-12 22:59:04 +0900

    3人称視点切り替え機能をメニューバーに追加

--- src/react-components/preferences-screen.js
+++ src/react-components/preferences-screen.js
@@ -23,7 +23,7 @@ import {
 } from "../utils/screen-orientation-utils";
 import { AAModes } from "../constants";
 import { isLockedDownDemoRoom } from "../utils/hub-utils";
-
+import { CAMERA_MODE_THIRD_PERSON_VIEW, CAMERA_MODE_FIRST_PERSON } from "../systems/camera-system";
 import dropdownArrowUrl from "../assets/images/dropdown_arrow.png";
 import dropdownArrow2xUrl from "../assets/images/dropdown_arrow@2x.png";
 import { PermissionNotification } from "./room/PermissionNotifications";
@@ -476,6 +476,10 @@ const preferenceLabels = defineMessages({
   enableDynamicShadows: {
     id: "preferences-screen.preference.enable-dynamic-shadows",
     defaultMessage: "Enable Real-time Shadows"
+  },
+  enableThirdPersonView: {
+    id: "preferences-screen.preference.enable-third-person-view",
+    defaultMessage: "Enable Third-Person View"
   },
   disableAutoPixelRatio: {
     id: "preferences-screen.preference.disable-auto-pixel-ratio",
@@ -999,6 +1003,10 @@ class PreferencesScreen extends Component {
       this.state.canVoiceChat &&
         this.mediaDevicesManager.startMicShare({ updatePrefs: false }).then(this.updateMediaDevices);
     }
+    const { enableThirdPersonView } = this.props.store.state.preferences;
+    this.props.scene.systems["hubs-systems"].cameraSystem.setMode(
+      enableThirdPersonView ? CAMERA_MODE_THIRD_PERSON_VIEW : CAMERA_MODE_FIRST_PERSON
+    );
   }
 
   createSections() {
@@ -1349,6 +1357,11 @@ class PreferencesScreen extends Component {
           {
             key: "enableDynamicShadows",
             prefType: PREFERENCE_LIST_ITEM_TYPE.CHECK_BOX
+          },
+          {
+            key: "enableThirdPersonView",
+            prefType: PREFERENCE_LIST_ITEM_TYPE.CHECK_BOX,
+            defaultBool: false
           },
           {
             key: "disableAutoPixelRatio",
